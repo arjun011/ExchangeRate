3eb2ee2e261829524faadd8a413feacee0da29a2
Arjun Patel <arjun.patel@nascentinfo.com>
2022-04-21 10:34:15 +0100

    Initial Commit

---
+++ ExchangeRate/ExchangeRateApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExchangeRateApp.swift
+//  ExchangeRate
+//
+//  Created by Arjun on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
